2cd8e6e317717e8b8b107ac709f6d7e28a2791ae
EvAlex <evalexhimself@gmail.com>
2015-09-04 12:29:25 +0300

    source maps

--- kurento-rtsp2many/gulpfile.js
+++ kurento-rtsp2many/gulpfile.js
@@ -1,17 +1,22 @@
 ﻿
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', ['server-ts']);
 
 gulp.task('server-ts', function () {
 	var tsResult = gulp.src(['./typings/**/*.d.ts', './server.ts'])
+		//.pipe(sourcemaps.init())
 		.pipe(ts({
 			noImplicitAny: false,
 			target: 'ES5',
 			outFile: 'server.js',
 			out: 'server.js',
+			sortOutput: true,
 		}));
 	
-	return tsResult.js.pipe(gulp.dest('./'));
+	return tsResult.js
+		//.pipe(sourcemaps.write())
+		.pipe(gulp.dest('./'));
 });
