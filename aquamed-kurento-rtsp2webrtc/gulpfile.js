8f26cd1fe1b0f2d75f7eadea1a9d484295ef3ef1
EvAlex <evalexhimself@gmail.com>
2015-09-11 11:04:18 +0300

    AquaMed sample: added package distr gulp task.

--- aquamed-kurento-rtsp2webrtc/gulpfile.js
+++ aquamed-kurento-rtsp2webrtc/gulpfile.js
@@ -3,18 +3,25 @@ var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('default', function () {
-    var tsResult = gulp.src(['./typings/**/*.d.ts', './script.ts'])
-        .pipe(sourcemaps.init())
-		.pipe(ts({
-		    noImplicitAny: false,
-		    target: 'ES5',
-		    outFile: 'script.js',
-		    out: 'script.js',
-		    sortOutput: true,
-		}));
+gulp.task('default', ['sample', 'distr']);
 
-    return tsResult.js
-        .pipe(sourcemaps.write({ includeContent: false, sourceRoot: './' }))
-		.pipe(gulp.dest('./'));
-});
+gulp.task('sample', getCompileTypeScriptTask('./script.ts', 'script.js'));
+gulp.task('distr', getCompileTypeScriptTask('./src/KurentoRtspStreamingManager.ts', './distr/KurentoRtspStreamingManager.js'));
+
+function getCompileTypeScriptTask(entranceFilePath, outFilePath, includeSourcesInMaps) {
+    return function () {
+        var tsResult = gulp.src(['./typings/**/*.d.ts', entranceFilePath])
+            .pipe(sourcemaps.init())
+            .pipe(ts({
+                noImplicitAny: false,
+                target: 'ES5',
+                outFile: outFilePath,
+                out: outFilePath,
+                sortOutput: true,
+            }));
+
+        return tsResult.js
+            .pipe(sourcemaps.write({ includeContent: false, sourceRoot: './' }))
+            .pipe(gulp.dest('./'));
+    }
+}
