05519347c07bae5d382d93c2eb459bb1c7889c34
Denis Bekasov <bekasovdenis@gmail.com>
2015-10-15 17:59:13 +0300

    add kurentomonitor files...moore.

---
+++ kurento-rtsp2many/server/KurentoMonitor/MonitApiClient.ts
@@ -0,0 +1,14 @@
+import MonitUrl = require('./MonitUrl');
+import MonitStatus = require('./MonitStatus');
+
+
+class MonitApiClient{
+	getMonitStatus(url: MonitUrl): Promise<MonitStatus>{	
+		return null;
+	}
+	
+	
+	
+}
+
+export = MonitApiClient;
