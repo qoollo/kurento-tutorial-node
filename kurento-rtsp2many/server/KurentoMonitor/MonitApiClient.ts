e2f826877f1c18270b1cf9f87c04f34018c897b6
artem.stolyarenko <roguenalan@gmail.com>
2015-10-16 04:41:57 +0300

    Made a callback version of MonitApiClient (didn't get how to make it on promises)

--- kurento-rtsp2many/server/KurentoMonitor/MonitApiClient.ts
+++ kurento-rtsp2many/server/KurentoMonitor/MonitApiClient.ts
@@ -1,9 +1,36 @@
+import http = require('http');
+import xml2js = require('xml2js');
 import MonitUrl = require('./MonitUrl');
 import MonitStatus = require('./MonitStatus');
 
 
-class MonitApiClient{
-	getMonitStatus(url: MonitUrl): Promise<MonitStatus>{	
+class MonitApiClient {	
+	getMonitStatus(url: MonitUrl): Promise<MonitStatus> {
+		var options = {
+			host: url.monitUrl.host,
+			path: url.monitUrl.path
+		};
+		var monitStatus: MonitStatus;
+		
+		http.get(options, response => {
+			var xmlSrc: string = '';
+			
+			response.on('data', chunk => {
+				xmlSrc += chunk;
+			});
+			response.on('error', error => {
+				return console.log(error);
+			});
+			response.on('end', () => 
+			{
+				var src = xml2js.parseString(xmlSrc, (err, result) => {
+					if (err != null)
+						return console.log(err);
+					monitStatus = new MonitStatus(result.monit); 
+				});
+			});
+		});
+		
 		return null;
 	}
 	
