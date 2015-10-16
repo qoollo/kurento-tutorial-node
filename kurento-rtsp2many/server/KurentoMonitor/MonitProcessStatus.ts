8194beeedc795c0ed7474959838437d0770f72ce
artem.stolyarenko <roguenalan@gmail.com>
2015-10-16 18:01:41 +0300

    Reworked parsing, added more fields to MonitProcessStatus

--- kurento-rtsp2many/server/KurentoMonitor/MonitProcessStatus.ts
+++ kurento-rtsp2many/server/KurentoMonitor/MonitProcessStatus.ts
@@ -1,13 +1,18 @@
 class MonitProcessStatus {
 	public uptime: number;
 	public memoryPercent: number;
-	public cpuPercent: number;
+	public cpuLoad: number;
+	public pid: number;
+	public ppid: number;
+	public children: number;
 	
 	public constructor(serviceSrc: any) {
-		this.uptime = serviceSrc.uptime;
-		this.memoryPercent = serviceSrc.memory.percent;
-		this.cpuPercent = serviceSrc.cpu.percent;
-		this.uptime = serviceSrc;
+		this.uptime = serviceSrc.uptime[0];
+		this.memoryPercent = serviceSrc.memory[0].percent[0];
+		this.cpuLoad = serviceSrc.cpu[0].percent[0];
+		this.pid = serviceSrc.pid[0];
+		this.ppid = serviceSrc.ppid[0];
+		this.children = serviceSrc.children[0];
 	}
 }
 
--- kurento-rtsp2many/server/KurentoMonitor/MonitSystemStatus.ts
+++ kurento-rtsp2many/server/KurentoMonitor/MonitSystemStatus.ts
@@ -2,17 +2,16 @@ import MonitUrl = require('./MonitUrl');
 
 class MonitSystemStatus {
 	public uptime: number;
-	public monitUrl: MonitUrl;
 	public memoryPercent: number;
 	public cpuLoad: number;
 
 	public constructor(monitSrc: any)  {
-			this.uptime = monitSrc.server.uptime;
-			this.monitUrl = monitSrc.server.httpd.address + monitSrc.server.httpd.port;
+			var server = monitSrc.server[0];
+			this.uptime = server.uptime[0];
 			
-			var service = monitSrc.service.filter((value, index, array) => value.$.type == '5')[0];
-			this.memoryPercent = service.memory.percent;
-			this.cpuLoad = service.cpu.user + service.cpu.system;
+			var service = monitSrc.service.filter((value, index, array) => value.$.type == '5')[0].system[0];
+			this.memoryPercent = service.memory[0].percent[0];
+			this.cpuLoad = service.cpu[0].user[0] + service.cpu[0].system[0];
 	}
 }
 
