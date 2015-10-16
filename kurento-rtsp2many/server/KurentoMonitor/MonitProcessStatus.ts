6a04a788918a1e24998622aca9d6068d3c931fc7
artem.stolyarenko <roguenalan@gmail.com>
2015-10-16 18:22:02 +0300

    Added parse functions

--- kurento-rtsp2many/server/KurentoMonitor/MonitProcessStatus.ts
+++ kurento-rtsp2many/server/KurentoMonitor/MonitProcessStatus.ts
@@ -7,12 +7,12 @@ class MonitProcessStatus {
 	public children: number;
 	
 	public constructor(serviceSrc: any) {
-		this.uptime = serviceSrc.uptime[0];
-		this.memoryPercent = serviceSrc.memory[0].percent[0];
-		this.cpuLoad = serviceSrc.cpu[0].percent[0];
-		this.pid = serviceSrc.pid[0];
-		this.ppid = serviceSrc.ppid[0];
-		this.children = serviceSrc.children[0];
+		this.uptime = parseInt(serviceSrc.uptime[0], 10);
+		this.memoryPercent = parseFloat(serviceSrc.memory[0].percent[0]);
+		this.cpuLoad = parseFloat(serviceSrc.cpu[0].percent[0]);
+		this.pid = parseInt(serviceSrc.pid[0], 10);
+		this.ppid = parseInt(serviceSrc.ppid[0], 10);
+		this.children = parseInt(serviceSrc.children[0], 10);
 	}
 }
 
--- kurento-rtsp2many/server/KurentoMonitor/MonitSystemStatus.ts
+++ kurento-rtsp2many/server/KurentoMonitor/MonitSystemStatus.ts
@@ -7,11 +7,11 @@ class MonitSystemStatus {
 
 	public constructor(monitSrc: any)  {
 			var server = monitSrc.server[0];
-			this.uptime = server.uptime[0];
+			this.uptime = parseInt(server.uptime[0], 10);
 			
 			var service = monitSrc.service.filter((value, index, array) => value.$.type == '5')[0].system[0];
-			this.memoryPercent = service.memory[0].percent[0];
-			this.cpuLoad = service.cpu[0].user[0] + service.cpu[0].system[0];
+			this.memoryPercent = parseFloat(service.memory[0].percent[0]);
+			this.cpuLoad = parseFloat(service.cpu[0].user[0]) + parseFloat(service.cpu[0].system[0]);
 	}
 }
 
