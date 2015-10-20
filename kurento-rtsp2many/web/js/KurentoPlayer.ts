b80426fda22d2803981150b8c1c7bc304d18d038
EvAlex <evalexhimself@gmail.com>
2015-10-20 10:24:42 +0300

    [KurentoHub] added error event to KurentoPlayer on client

--- kurento-rtsp2many/web/js/KurentoPlayer.ts
+++ kurento-rtsp2many/web/js/KurentoPlayer.ts
@@ -4,6 +4,7 @@ class KurentoPlayer /*extends EventTarget*/ {
     constructor(private _streamUrl: string, private webRtcPeer: Kurento.Utils.IWebRtcPeer) {
         //super();
         this._src = webRtcPeer.remoteVideo.src;
+        this.webRtcPeer.onerror = err => this.raiseEvent(KurentoPlayer.events.error, err)
     }
     
     public get src(): string {
@@ -21,6 +22,35 @@ class KurentoPlayer /*extends EventTarget*/ {
 
     public stop(): Promise<any> {
         return Promise.reject('Not implemented');
+    }
+    
+    public dispose(): Promise<any> {
+        return Promise.resolve(this.webRtcPeer.dispose());
+    }
+    
+    private raiseEvent(errorType: { name: string, listeners: any[] }, ev): void {
+        errorType.listeners.forEach(l => l(ev));
+    }
+    
+    public addEventListener(eventName: string, listener: (ev) => void): void {
+        if (eventName in KurentoPlayer.events)
+            KurentoPlayer.events[eventName].listeners.push(listener);
+        else
+            throw new Error('KurentoPlayer does not support event ' + eventName);
+    }
+    
+    public removeEventListener(eventName: string, listener: (ev) => void): void {
+        if (eventName in KurentoPlayer.events)
+            KurentoPlayer.events[eventName].listeners.splice(KurentoPlayer.events[eventName].listeners.indexOf(listener), 1);
+        else
+            throw new Error('KurentoPlayer does not support event ' + eventName);
+    }
+    
+    public static events = {
+        error: {
+            name: 'error',
+            listeners: []
+        }
     }
 }
 
