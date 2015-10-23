4c2adc823a01c628d16992aea2562d4f9f5b4c51
Donnatello <11.Donnatello@gmail.com>
2015-10-24 01:41:01 +0300

    [KurentoHub/Storage] Db

--- kurento-rtsp2many/server/Storage/KurentoHubDb.ts
+++ kurento-rtsp2many/server/Storage/KurentoHubDb.ts
@@ -10,9 +10,9 @@ interface IMonit extends Monitor.IMonit, mongoose.Document { }
 
 class KurentoHubDb {
 	
-	private videoConsumers : mongoose.Model<IVideoConsumer>; 
-	private kurentoServers  : mongoose.Model<IKurentoServer>;
-	private monitModels : mongoose.Model<IMonit>;
+	private videoConsumers : mongoose.Model<IVideoConsumer> = Model.VideoConsumer; 
+	private kurentoServers  : mongoose.Model<IKurentoServer> = Model.KurentoServer;
+	private monitModels : mongoose.Model<IMonit> = Model.Monit;
 	
 	constructor() {
 		
@@ -28,47 +28,7 @@ class KurentoHubDb {
 		db.once('open', () => {
     		logger.info("Connected to database.");
 		});
-		
-		var videoConsumerSchema : mongoose.Schema = new mongoose.Schema({
-    		registerTime: {
-      			type: Date,
-      			default: Date.now
-    		},
-    		streamConnections: [{
-				streamUrl: String,
-				kurentoServerId: String,
-				connectTime: {
-            		type: Date,
-            		default: Date.now
-        		},
-			}],
-		});
-		this.videoConsumers = mongoose.model<IVideoConsumer>("VideoConsumer", videoConsumerSchema);
-	
-		var kurentoServerSchema : mongoose.Schema = new mongoose.Schema({
-   			__id: {
-				   type: String,
-				   unique: true,
-			},
-			url: {
-				type : String,	
-				unique: true,
-			} 
-		});
-		this.kurentoServers = mongoose.model<IKurentoServer>("KurentoServer", kurentoServerSchema);
-		
-		var monitSchema : mongoose.Schema = new mongoose.Schema({
-    		currentStatus: {
-      			state: Number,
-				status : Number,
-				isMonitored : Boolean,
-				isPending : Boolean,
-				uptime : Number,
-				memory : Number,
-				cpu : Number,
-			}
-		});
-		this.monitModels = mongoose.model<IMonit>("Monit", monitSchema);		
+				
 	}
 	
 	
@@ -85,7 +45,7 @@ class KurentoHubDb {
                         return res;
                     };
 
-                var kurentoSer = new this.kurentoServers({
+                var kurentoSer = new Model.KurentoServer({
                     __id: i.toString(),
                     url: getAddress(e)
                 })
@@ -105,13 +65,12 @@ class KurentoHubDb {
 	
 	getKurentoServers(): Promise<Storage.IKurentoServer[]> {
 		return new Promise((resolve, reject) => {
-			this.kurentoServers.find((err, servers) => { 
-				servers.forEach((s) => s.re)
+			Model.KurentoServer.find((err, servers) => { 
 				
 				if (err)
 					reject(err); // И СЮДА!
 					
-				resolve();
+				resolve(servers);
 			})
 		});
 	}
