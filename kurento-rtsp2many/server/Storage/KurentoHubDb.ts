2d0277e945dc41579a0ead7376ed994899802c59
Donnatello <11.Donnatello@gmail.com>
2015-10-25 01:45:24 +0300

    [KurentoHub/Storage] connect method and validation.

--- kurento-rtsp2many/server/Storage/KurentoHubDb.ts
+++ kurento-rtsp2many/server/Storage/KurentoHubDb.ts
@@ -1,39 +1,61 @@
-import AppConfig = require('../AppConfig'); 
+import AppConfig = require('../AppConfig');
 import mongoose = require('mongoose');
 import logger = require('../Logger');
 import Monitor = require('../KurentoMonitor/Monitor');
 import Model = require('./Model');
-
-interface IVideoConsumer extends Storage.IVideoConsumer, mongoose.Document { }
-interface IKurentoServer extends Storage.IKurentoServer, mongoose.Document  { }
-interface IMonit extends Monitor.IMonit, mongoose.Document { }
+import Document = require('./Document.ts');
+import DbState = require('./DbState');
 
 class KurentoHubDb {
 	
-	private videoConsumers : mongoose.Model<IVideoConsumer> = Model.VideoConsumer; 
-	private kurentoServers  : mongoose.Model<IKurentoServer> = Model.KurentoServer;
-	private monitModels : mongoose.Model<IMonit> = Model.Monit;
-	
+	private ACTION_WITH_DISCONNECTED_DB_ERROR = 'No connection to the database.';
+
+	public state: DbState;
+	private lastConnectionPromise: Promise<any>;
+
 	constructor() {
-		
+		this.state = DbState.Disconnected;
+	}
+
+	connect(reconect : boolean = false): Promise<any> {
 		logger.info("Trying connect to database.");
 		
-		mongoose.connect(AppConfig.config.mongodb.uri);
-		var db = mongoose.connection;
-		
-		db.on('error', (err) => {
-    		logger.error('Connection error:', err.message);
-		});
-		
-		db.once('open', () => {
-    		logger.info("Connected to database.");
-		});
-				
+		if (this.state == DbState.Disconnected || reconect) {
+			
+			this.lastConnectionPromise = new Promise((resolve, reject) => {
+				logger.info("Creating new connection to KurentoDb.");
+
+				this.state = DbState.Connecting;
+				mongoose.connect(AppConfig.config.mongodb.uri);
+
+				var db = mongoose.connection;
+
+				db.on('error', (err) => {
+					logger.error('Connection to db error:', err.message);
+					reject(err);
+
+					this.state = DbState.Disconnected;
+					this.lastConnectionPromise = null;
+				});
+
+				db.once('open', () => {
+					logger.info("Connected to database.");
+					resolve();
+					
+					this.state = DbState.Connected;
+				});
+			})
+		}
+		else
+			logger.warn("Already connected to database.");	
+			
+		return this.lastConnectionPromise;
 	}
-	
-	
-	
+
 	seedData(): Promise<any> {
+		if (this.state != DbState.Connected)
+			return Promise.reject(this.ACTION_WITH_DISCONNECTED_DB_ERROR);
+		
 		return new Promise((resolve, reject) => {
             AppConfig.config.kurentoMediaServer.defaultInstances.forEach((e, i) => {
                 var template = AppConfig.config.kurentoMediaServer.wsUrlTemplate,
@@ -49,24 +71,26 @@ class KurentoHubDb {
                     __id: i.toString(),
                     url: getAddress(e)
                 })
-					
-				kurentoSer.save((err, server : IKurentoServer) => {
+
+				kurentoSer.save((err, server: Document.IKurentoServerDocument) => {
 					if (err)
 						err;
-						//нормального обработчика сюда! ТУТ Pi@#&*
+					//нормального обработчика сюда! ТУТ Pi@#&*
 
-				}) 
+				})
 			})
-			
-			
+
 			resolve();
 		});
 	}
-	
+
 	getKurentoServers(): Promise<Storage.IKurentoServer[]> {
+		if (this.state != DbState.Connected)
+			return Promise.reject(this.ACTION_WITH_DISCONNECTED_DB_ERROR);
+		
 		return new Promise((resolve, reject) => {
-			Model.KurentoServer.find((err, servers) => { 
-				
+			Model.KurentoServer.find((err, servers) => {
+
 				if (err)
 					reject(err); // И СЮДА!
 					
@@ -75,11 +99,12 @@ class KurentoHubDb {
 		});
 	}
 	
-	//private kurentoServers: Storage.IKurentoServer[] = [];
-	
 	getRegisteredVideoConsumers(): Promise<Storage.IVideoConsumer[]> {
+		if (this.state != DbState.Connected)
+			return Promise.reject(this.ACTION_WITH_DISCONNECTED_DB_ERROR);
+		
 		return new Promise((resolve, reject) => {
-			this.videoConsumers.find((err, consumers) => { 
+			Model.VideoConsumer.find((err, consumers) => {
 				if (err)
 					reject(err); // И СЮДА!
 					
@@ -87,31 +112,32 @@ class KurentoHubDb {
 			})
 		});
 	}
-	
+
 	registerVideoConsumer(): Promise<Storage.IVideoConsumer> {
-		var videoConsumera: Storage.IVideoConsumer = { 
-			clientId: '' , //this.registeredVideoConsumers.reduce((p, c) => Math.max(p, c.clientId), 1) + 1,
+		if (this.state != DbState.Connected)
+			return Promise.reject(this.ACTION_WITH_DISCONNECTED_DB_ERROR);
+		
+		var videoConsumera: Storage.IVideoConsumer = {
+			clientId: '',
 			registerTime: new Date(),
 			streamConnections: []
 		};
-		
-		var videoConsumer = new this.videoConsumers({ 
+
+		var videoConsumer = new Model.VideoConsumer({
 			registerTime: new Date(),
 			streamConnections: []
 		})
-		
+
 		return new Promise((resolve, reject) => {
-			videoConsumer.save((err) => { 
+			videoConsumer.save((err) => {
 				if (err)
 					reject(err); // И СЮДА!
 				
-				videoConsumer.clientId = videoConsumer.id;	
+				videoConsumer.clientId = videoConsumer.id;
 				resolve(<Storage.IVideoConsumer>videoConsumer);
 			})
 		});
 	}
-	
-	//private registeredVideoConsumers: Storage.IVideoConsumer[] = [];
 	
 }
 
